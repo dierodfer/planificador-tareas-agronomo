f87e007c6e55a480b318564dd66bfa27ebe5b10b
Diego Rodriguez Fernandez <dierodfer6@gmail.com>
2019-02-05 12:20:28 +0100

    Nuevas caracteristicas para PWA

--- plantilla/angular-project/src/environments/environment.prod.ts
+++ plantilla/angular-project/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAQEcSZQ-hQnew_jCRE21flK_g9Kt5L7iw",
-    authDomain: "angular-222712.firebaseapp.com",
-    databaseURL: "https://angular-222712.firebaseio.com",
-    projectId: "angular-222712",
-    storageBucket: "angular-222712.appspot.com",
-    messagingSenderId: "364563909467"
+    apiKey: 'AIzaSyAQEcSZQ-hQnew_jCRE21flK_g9Kt5L7iw',
+    authDomain: 'angular-222712.firebaseapp.com',
+    databaseURL: 'https://angular-222712.firebaseio.com',
+    projectId: 'angular-222712',
+    storageBucket: 'angular-222712.appspot.com',
+    messagingSenderId: '364563909467'
   }
 };
--- plantilla/angular-project/src/environments/environment.ts
+++ plantilla/angular-project/src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAQEcSZQ-hQnew_jCRE21flK_g9Kt5L7iw",
-    authDomain: "angular-222712.firebaseapp.com",
-    databaseURL: "https://angular-222712.firebaseio.com",
-    projectId: "angular-222712",
-    storageBucket: "angular-222712.appspot.com",
-    messagingSenderId: "364563909467"
+    apiKey: 'AIzaSyAQEcSZQ-hQnew_jCRE21flK_g9Kt5L7iw',
+    authDomain: 'angular-222712.firebaseapp.com',
+    databaseURL: 'https://angular-222712.firebaseio.com',
+    projectId: 'angular-222712',
+    storageBucket: 'angular-222712.appspot.com',
+    messagingSenderId: '364563909467'
   }
 };
 
